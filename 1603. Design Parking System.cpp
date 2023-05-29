8ec41af0f0c54fb6a0a950638d8b2017fba9484a
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2023-05-29 09:23:53 +0530

    Create 1603. Design Parking System.cpp

---
+++ 1603. Design Parking System.cpp
@@ -0,0 +1,16 @@
+LeetCode
+1603. Design Parking System
+
+class ParkingSystem {
+ public:
+  ParkingSystem(int big, int medium, int small) {
+    count = {big, medium, small};
+  }
+
+  bool addCar(int carType) {
+    return count[carType - 1]-- > 0;
+  }
+
+ private:
+  vector<int> count;
+};
