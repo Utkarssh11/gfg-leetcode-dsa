3a215f280ff564ea6bf4c12e2c4da64a1e67a8c3
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2023-06-03 10:20:18 +0530

    Create Find Maximum Equal sum of Three Stacks.cpp

---
+++ Find Maximum Equal sum of Three Stacks.cpp
@@ -0,0 +1,21 @@
+GFG
+Find Maximum Equal sum of Three Stacks
+
+class Solution{
+public:
+    int maxEqualSum(int N1,int N2,int N3,vector<int> &S1,vector<int> &S2,vector<int> &S3){
+        int sum1 = 0, sum2 = 0, sum3 = 0;
+        for(auto i : S1) sum1 += i;
+        for(auto i : S2) sum2 += i;
+        for(auto i : S3) sum3 += i;
+        
+        int i = 0, j = 0, k = 0;
+        while(i < N1 and j < N2 and k < N3){
+            if(sum1 == sum2 and sum1 == sum3) return sum1;
+            if(sum1 > sum3 or sum1 > sum2) sum1 -= S1[i++];
+            else if(sum2 > sum3 or sum2 > sum1) sum2 -= S2[j++];
+            else if(sum3 > sum1 or sum3 > sum2) sum3 -= S3[k++];
+        }
+        return 0;
+    }
+};
