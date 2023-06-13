7dd4d87fdab7da88ae1b1d6ea61d46a7b8db71a9
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2023-06-13 11:01:18 +0530

    Create 2352. Equal Row and Column Pairs.java

---
+++ 2352. Equal Row and Column Pairs.java
@@ -0,0 +1,21 @@
+LeetCode
+2352. Equal Row and Column Pairs
+
+class Solution {
+  public int equalPairs(int[][] grid) {
+    final int n = grid.length;
+    int ans = 0;
+
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < n; ++j) {
+        int k = 0;
+        for (; k < n; ++k)
+          if (grid[i][k] != grid[k][j])
+            break;
+        if (k == n) // R[i] == C[j]
+          ++ans;
+      }
+
+    return ans;
+  }
+}
