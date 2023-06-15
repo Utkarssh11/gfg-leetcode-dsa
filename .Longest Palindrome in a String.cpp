e6ea4aa996e5b8d760d6465142fa3940d9fb107d
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2023-06-15 10:40:36 +0530

    Create .Longest Palindrome in a String.cpp

---
+++ .Longest Palindrome in a String.cpp
@@ -0,0 +1,52 @@
+GFG
+
+Longest Palindrome in a String
+
+
+
+class Solution {
+  public:
+    string longestPalin (string S) {
+        int n=S.length();
+        int MinInd=0,MaxLen=1;
+        for(int center=0;center<n;center++)
+        {
+            int i=center,j=center;
+            while(i>=0 && j<n && S[i]==S[j])
+            {
+                i--;
+                j++;
+            }
+            i++;
+            j--;
+            if(j-i+1>MaxLen)
+            {
+                MaxLen=j-i+1;
+                MinInd=i;
+            }
+            else if(j-i+1==MaxLen)
+            {
+                MinInd=min(MinInd,i);
+            }
+
+            i=center,j=center+1;
+            while(i>=0 && j<n && S[i]==S[j])
+            {
+                i--;
+                j++;
+            }
+            i++;
+            j--;
+            if(j-i+1>MaxLen)
+            {
+                MaxLen=j-i+1;
+                MinInd=i;
+            }
+            else if(j-i+1==MaxLen)
+            {
+                MinInd=min(MinInd,i);
+            }
+        }
+        return S.substr(MinInd,MaxLen);
+    }
+};
