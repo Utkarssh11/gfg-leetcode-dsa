0897fbf624fb702e6e45d557b6c78ba3895e66cf
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2023-06-20 11:01:43 +0530

    Create Matchsticks game.java

---
+++ Matchsticks game.java
@@ -0,0 +1,16 @@
+GFG
+Matchsticks game
+
+
+
+class Solution {
+    static int matchGame(Long N) {
+           if(N%5==0){
+            return -1;
+
+        }
+
+        int rem=(int)(N%5);
+        return rem;
+    }
+};
