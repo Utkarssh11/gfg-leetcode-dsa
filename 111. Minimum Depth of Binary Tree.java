bd63f40eb0ea5875e0852e1adff24b9c9f4a5922
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2023-07-10 10:25:57 +0530

    Create 111. Minimum Depth of Binary Tree.java

---
+++ 111. Minimum Depth of Binary Tree.java
@@ -0,0 +1,19 @@
+LeetCode
+
+111. Minimum Depth of Binary Tree
+
+  
+
+class Solution {
+  public int minDepth(TreeNode root) {
+    if (root == null)
+      return 0;
+    if (root.left == null)
+      return minDepth(root.right) + 1;
+    if (root.right == null)
+      return minDepth(root.left) + 1;
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+  }
+}
+
+
