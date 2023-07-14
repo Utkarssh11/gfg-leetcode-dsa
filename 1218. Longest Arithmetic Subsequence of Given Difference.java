7098edf008b2b6985ec4e3047b605695ff89f1e2
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2023-07-14 12:36:41 +0530

    Create 1218. Longest Arithmetic Subsequence of Given Difference.java

---
+++ 1218. Longest Arithmetic Subsequence of Given Difference.java
@@ -0,0 +1,17 @@
+1218. Longest Arithmetic Subsequence of Given Difference
+LeetCode
+
+
+  class Solution {
+  public int longestSubsequence(int[] arr, int difference) {
+    int ans = 0;
+    Map<Integer, Integer> lengthAt = new HashMap<>();
+
+    for (final int a : arr) {
+      lengthAt.put(a, lengthAt.getOrDefault(a - difference, 0) + 1);
+      ans = Math.max(ans, lengthAt.get(a));
+    }
+
+    return ans;
+  }
+}
