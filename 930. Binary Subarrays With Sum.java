bf5371d24e5cf512a77bdae6005c0a686a0d4155
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2024-03-14 14:04:38 +0530

    Create 930. Binary Subarrays With Sum.java

---
+++ 930. Binary Subarrays With Sum.java
@@ -0,0 +1,24 @@
+Leetcode Java Solution
+
+
+  930. Binary Subarrays With Sum
+
+  class Solution {
+  public int numSubarraysWithSum(int[] nums, int goal) {
+    int ans = 0;
+    int prefix = 0;
+    // {prefix: number of occurrence}
+    Map<Integer, Integer> count = new HashMap<>();
+    count.put(0, 1);
+
+    for (final int num : nums) {
+      prefix += num;
+      final int key = prefix - goal;
+      if (count.containsKey(key))
+        ans += count.get(key);
+      count.merge(prefix, 1, Integer::sum);
+    }
+
+    return ans;
+  }
+}
