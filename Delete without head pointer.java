e418d050c3f2292a718af3b44fe27b61099bc2cf
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2024-03-16 14:04:13 +0530

    Create Delete without head pointer.java

---
+++ Delete without head pointer.java
@@ -0,0 +1,11 @@
+GFG java Solution
+
+  Delete without head pointer
+
+
+  class Solution{
+    void deleteNode(Node del_node) {
+         del_node.data=del_node.next.data;
+         del_node.next=del_node.next.next;
+    }
+}
